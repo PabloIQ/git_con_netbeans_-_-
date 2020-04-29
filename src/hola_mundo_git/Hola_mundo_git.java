f33bec65156355080170a9ec2d117155c73b9186
PabloIQ <cpablop1@miumg.edu.gt>
2020-04-29 15:55:38 -0600

    He programado una suma que muestre el resultado en un JOptionPane

--- src/hola_mundo_git/Hola_mundo_git.java
+++ src/hola_mundo_git/Hola_mundo_git.java
@@ -1,9 +1,16 @@
 package hola_mundo_git;
 
+import javax.swing.JOptionPane;
+
 public class Hola_mundo_git {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        int a, b, result;
+        a = 3;
+        b = 5;
+        result = a + b;
+        JOptionPane.showMessageDialog(null, "La suma es: " + result);
     }
     
 }
